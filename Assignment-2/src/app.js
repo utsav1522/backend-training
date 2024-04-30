36670c0741be9c8130f784549d86fc7b607e52cf
utsav1522 <utsav.jain@successive.tech>
2024-04-30 18:49:38 +0530

    review work

--- Assignment-2/src/app.js
+++ Assignment-2/src/app.js
@@ -1,16 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./router.js";
+import cookieParser from "cookie-parser";
 
 const env = dotenv.config().parsed;
-
 const app = express();
 
 let port = env.PORT || 4000;
 app.use("/api", router);
+app.use(cookieParser());
 
 app.use("/", (req, res) => {
-  res.send("<h1>Backend Training Assignment...</h1>");
+  res.cookie("cookieName", "cookieOne", {
+    expires: new Date(Date.now() + 900000),
+  });
+
+  res.cookie("theme", "dark", {
+    expires: new Date(Date.now() + 900000),
+  });
+
+  console.log("Unsigned Cookies; ", req.cookies);
+  console.log(req.headers.cookie);
+  res.send("Backend Training Assignment...");
 });
 
 app.listen(port, function () {
